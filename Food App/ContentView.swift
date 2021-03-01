e69b03d0c612864de033f74cd1ca395cab805bf5
NeoFreya <neonblackcat19@gmail.com>
2021-03-01 11:13:25 +0800

    Initial Commit

---
+++ Food App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food App
+//
+//  Created by Abdul Halim on 01/03/21.
+//  Copyright © 2021 NEONFACT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
